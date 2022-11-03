2531d3029a0252ac0c51e7eb714975b4273ddd39
KAYAMAc <30427804+KAYAMAc@users.noreply.github.com>
2022-11-03 18:14:51 +0900

    Update solution_polished.java

--- solution_polished.java
+++ solution_polished.java
@@ -11,69 +11,68 @@ a a b b c d e f b
 001 101 002 102 003 004 005 006 202
 */
 package Solutions;
-
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+//import java.util.Scanner;
+//import java.util.ArrayList;
 public class solution_polished {
+		public static void main(String[] args){
+			Scanner input = new Scanner(System.in);
+			String number = input.nextLine();
+			int inputNumber = Integer.parseInt(number);
+			for(int x = 0;x < inputNumber;x++) {
+			    String inputCodes = input.nextLine();
+			    if(inputCodes == "") {
+				System.out.println("コードを入力してください。");
+				inputNumber++;
+				continue;
+			    }
+			    String[] ans = get_id(inputCodes.split(" "));
+			    for(int ind = 0;ind < ans.length;ind++) {
+				if(ind != ans.length-1) {
+					System.out.print(ans[ind] + " ");
+				}
+				else System.out.print(ans[ind] + "\n");
+			    }
+				// System.out.println("");
+			}
+			input.close();
+		}
 	
-		private static int existed_contains(String[] full_list, String s) {
-			int list_length=full_list.length;
-			for (int p=list_length-1;p>=0;p--) {
-				if(full_list[p].equalsIgnoreCase(s)) {
+		private static int existed_contains(String[] fullList, String s) {
+			int listLength = fullList.length;
+			for (int p = listLength-1;p >= 0;p--) {
+				if(fullList[p].equalsIgnoreCase(s)) {
 					return p;
 				}
 			}
 			return -1;
 		}
 		
-		private static String[] get_id(String [] new_codes) {
-			ArrayList<String> existed=new ArrayList<String>();
-		    ArrayList<String> ids=new ArrayList<String>();
-		    int new_id=1;
-		    for(String code:new_codes){
-		    	int index=existed_contains(existed.toArray(new String[0]),code);
-	    		String [] idss=ids.toArray(new String[0]);
-		    	if(index>=0) {
-		    		if(Integer.parseInt(idss[index])>=900) {
-		    			ids.add(String.format("%03d", new_id));
-	    	    		existed.add(code);
-		    			new_id++;
-		    		}
-		    		else {
-		    			ids.add(String.format("%03d", Integer.parseInt(idss[index])+100));
-		    			existed.add(code);
-		    		}
-		    	}
-		    	else if(index<0) {
-		    		ids.add(String.format("%03d", new_id));
-		    		existed.add(code);
-		    		new_id++;
-		    	}
-		    }
-		    return ids.toArray(new String[0]);
+		private static String[] get_id(String [] codes) {
+			ArrayList<String> existed = new ArrayList<String>();
+			ArrayList<String> ids=new ArrayList<String>();
+			int newId = 1;
+			for(String code : codes){
+				int index = existed_contains(existed.toArray(new String[0]),code);
+				String [] idss = ids.toArray(new String[0]);
+				if(index >= 0) {
+					if(Integer.parseInt(idss[index]) >= 900) {
+						ids.add(String.format("%03d", newId));
+						existed.add(code);
+						newId++;
+				}else {
+					ids.add(String.format("%03d", Integer.parseInt(idss[index]) + 100));
+					existed.add(code);
+				}
+				}else if(index < 0) {
+					ids.add(String.format("%03d", newId));
+					existed.add(code);
+					newId++;
+				}
+			}
+			return ids.toArray(new String[0]);
 		}
 		
-		public static void main(String[] args){
-			Scanner input=new Scanner(System.in);
-	        String number=input.nextLine();
-	        int input_number=Integer.parseInt(number);
-	        for(int x=0;x<input_number;x++) {
-	    	    String input_codes=input.nextLine();
-	    	    if(input_codes=="") {
-	    	    	System.out.println("コードを入力してください。");
-	    	    	input_number++;
-	    	    	continue;
-	    	    }
-	    	    String[] ans=get_id(input_codes.split(" "));
-	    	    for(int ind=0;ind<ans.length;ind++) {
-	    	    	if(ind!=ans.length-1) {
-	        	    	System.out.print(ans[ind]+" ");
-	    	    	}
-	    	    	else System.out.print(ans[ind]);
-	    	    }
-		    	System.out.println("");
-	        }
-	        input.close();
-		}
+		
 }
 
